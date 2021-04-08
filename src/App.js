365fbce2b2b68a111d67b530f005aa7f98788f73
Anna Lebid <annalez16@gmail.com>
2021-04-08 23:41:39 +0300

    add a SearchBox component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 import Header from './components/Header';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   
-  const getMovies = async () => {
+  const getMovies = async (searchValue) => {
     const omdbApiKey = process.env.REACT_APP_OMDB_API_KEY;   
-    const url = `http://www.omdbapi.com/?s=star wars&apikey=${omdbApiKey}`
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${omdbApiKey}`
     const response = await fetch(url);
     const responseJson = await response.json();    
-    setMovies(responseJson.Search)
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search)
+    }    
   }
 
   useEffect (() => {
-    getMovies();
-  }, []);
+    getMovies(searchValue);
+  }, [searchValue]);
 
   return (
     <div className = 'container-fluid movie-list'>
       <div className = 'row'>
         <Header header = 'Movies'/>
       </div>
+      <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       <div className = 'row'>
         <MovieList movies = {movies}/>
       </div>
